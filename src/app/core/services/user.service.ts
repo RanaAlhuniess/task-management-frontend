696dfd1a1cc2e13a2faea1d65630ae7ae0776952
Rana Alhuniess <rana.hanis93@gmail.com>
2023-01-16 12:54:19 +0300

    fix login issue

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -28,9 +28,9 @@ export class UserService {
   populate() {
     // If JWT detected, attempt to get & store user's info
     if (this.jwtService.getToken()) {
-      this.apiService.get('/user')
+      this.apiService.get('/users/me')
         .subscribe(
-          data => this.setAuth(data.user),
+          data => this.setAuth(data),
           err => this.purgeAuth()
         );
     } else {
